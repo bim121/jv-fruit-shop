13b1054cd76297bcf0a2472b43f37416968568ec
Vitaliy Tishik <vitisik54@gmail.com>
2025-11-05 17:33:11 +0200

    ref

--- src/main/java/core/basesyntax/service/impl/DataConverterImpl.java
+++ src/main/java/core/basesyntax/service/impl/DataConverterImpl.java
@@ -4,7 +4,6 @@ import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.DataConverter;
 import java.util.ArrayList;
 import java.util.List;
-
 public class DataConverterImpl implements DataConverter {
     @Override
     public List<FruitTransaction> convertToTransaction(List<String> lines) {
